5e0022a4b8430b113fd50cd4bf95f9f838d9baa0
IamHPark <heesoo.p1023@gmail.com>
2022-04-13 21:31:27 +0000

    try hard-coded for move command

--- client.js
+++ client.js
@@ -3,15 +3,36 @@ const net = require('net');
 // move connect function from play.js to client.js
 const connect = function () {
   const conn = net.createConnection({
-    host: '10.0.2.15',
+    host: '165.227.47.243',
     port: 50541
   });
 
   conn.setEncoding('utf-8');
 
+  conn.on('connect', () => {
+    conn.write("Successfully connected to game server!")
+  })
+
+  conn.on('connect', () => {
+    conn.write('Name: SOO');
+  });
+  
+  //hard-coding : command for moving 
+  // conn.on('connect', () => {
+  //     setTimeout( () => { conn.write("Move: up"); }, 1000)
+  // });
+  // conn.on('connect', () => {
+  //   let count = 0;
+  //   let interval = setInterval( () => {
+  //     count ++;
+  //     if( count === 10) {
+  //       clearInterval(interval);
+  //     }
+  //     conn.write("Move: up");
+  //   }, 500);
+  // });
+
   return conn;
 };
 
-module.exports = {
-  connect
-};
+module.exports = connect;
