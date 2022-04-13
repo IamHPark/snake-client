9b3eda5900b6af8f49c5fab81dbd0325624aad06
IamHPark <heesoo.p1023@gmail.com>
2022-04-13 22:08:05 +0000

    separate input module

--- play.js
+++ play.js
@@ -1,29 +1,9 @@
-const connect = require('./client');
+const { connect } = require('./client');
+const { setupInput } = require('./input');
 
 
 console.log("Connecting....");
 connect();
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf-8');
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.stdout.write('exit game\n');
-    process.exit();
-  }
-};
-
 setupInput();
 
-
-//
-// stdin.on('data', (key) => {
-//   process.stdout.write('.');
-// });
