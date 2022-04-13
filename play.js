d350ec50b6f532efc5cfa39835cdd83365678437
IamHPark <heesoo.p1023@gmail.com>
2022-04-13 22:00:08 +0000

    add event listener

--- play.js
+++ play.js
@@ -1,6 +1,29 @@
-const net = require('net');
-const { connect } = require('./client');
+const connect = require('./client');
 
 
 console.log("Connecting....");
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf-8');
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.stdout.write('exit game\n');
+    process.exit();
+  }
+};
+
+setupInput();
+
+
+//
+// stdin.on('data', (key) => {
+//   process.stdout.write('.');
+// });
