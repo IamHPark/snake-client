dcff62b8f0156247092d234a419f3aeb3819273b
IamHPark <heesoo.p1023@gmail.com>
2022-04-13 16:20:45 +0000

    separate net connection to client.js

--- play.js
+++ play.js
@@ -1,16 +1,6 @@
 const net = require('net');
+const { connect } = require('./client');
 
-// establish a connection with the game server 
-const connect = function () {
-  const conn = net.createConnection({
-    host: '10.0.2.15',
-    port: 50541
-  });
-
-  conn.setEncoding('utf-8');
-
-  return conn;
-};
 
 console.log("Connecting....");
 connect();
